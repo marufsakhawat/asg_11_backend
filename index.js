237c557ce145be2b691e4383533f0aa350ec09c0
Maruf Sakhawat <marufsakhawat@gmail.com>
2025-12-16 10:37:13 +0600

    DB info moved to .env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://asg_11:9blIoKk4FtGaHkut@cluster0.9zbfeju.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9zbfeju.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
